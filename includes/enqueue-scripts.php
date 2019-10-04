8af964a74aaa85e9045497c28087ae9c173aadf3
Mike England <mike.england@webdevstudios.com>
2019-10-04 14:39:51 -0400

    Added editor and front end style enqueues.

--- includes/enqueue-scripts.php
+++ includes/enqueue-scripts.php
@@ -1,10 +1,9 @@
 <?php
 
 /**
- * Enqueue block editor only JavaScript and CSS
+ * Enqueue block editor only JavaScript and Editor CSS
  *
  * @author Mike England <mike.england@webdevstudios.com>
- *
  */
 function gput_blocks_editor_scripts() {
 
@@ -17,5 +16,29 @@ function gput_blocks_editor_scripts() {
 		[ 'wp-i18n', 'wp-element', 'wp-blocks', 'wp-components', 'wp-editor' ],
 		filemtime( plugin_dir_path( __FILE__ ) . $blockPath )
 	);
+
+	wp_enqueue_style(
+		'gput-editor-styles',
+		plugins_url( '../build/editor.css', __FILE__ ),
+		array( 'wp-edit-blocks' ),
+		filemtime( plugin_dir_path( __FILE__ ) . '../build/editor.css' )
+	);
 }
 add_action( 'enqueue_block_editor_assets', 'gput_blocks_editor_scripts' );
+
+/**
+ * Enqueue Frontend Styles
+ *
+ * @author Mike England <mike.england@webdevstudios.com>
+ *
+ */
+function gput_register_block_styles() {
+
+	wp_enqueue_style(
+		'gput-styles',
+		plugins_url( '../build/style.css', __FILE__ ),
+		array( ),
+		filemtime( plugin_dir_path( __FILE__ ) . '../build/style.css' )
+	);
+}
+add_action( 'init', 'gput_register_block_styles' );
